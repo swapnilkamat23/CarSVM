0412d6379c8859d5376ab868c50f018edecd8b65
Maciej Krok <mkrok@metal.agh.edu.pl>
2012-03-28 14:20:20 +0200

    init

---
+++ src/carsvm/Model.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package carsvm;
+
+/**
+ *
+ * @author proteus
+ */
+public class Model {
+    
+}
